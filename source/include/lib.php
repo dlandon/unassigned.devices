092f54333cb0cad96443b9dda9bd5f71e07c441f
dlandon <dlandon@cinci.rr.com>
2016-02-10 17:36:21 -0500

    Release 2016.02.10
    Update GPL license and copyrights.

--- source/include/lib.php
+++ source/include/lib.php
@@ -1,5 +1,6 @@
-<?
-/* Copyright 2016, Dan Landon.
+<?PHP
+/* Copyright 2015, Guilherme Jardim
+ * Copyright 2016, Dan Landon
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2,
--- source/include/script.php
+++ source/include/script.php
@@ -1,5 +1,5 @@
-<?
-/* Copyright 2016, Dan Landon.
+<?PHP
+/* Copyright 2016, Dan Landon
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2,
