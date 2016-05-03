a6b3cae3fb9b1b31f4fe72ee3f56fa3726c92a24
dlandon <dlandon@cinci.rr.com>
2016-05-03 09:50:49 -0400

    Update 2016.05.03.
    Add nvme support.

--- source/include/lib.php
+++ source/include/lib.php
@@ -954,8 +954,8 @@ function get_unasigned_disks() {
 	// Get all devices by id.
 	foreach (listDir("/dev/disk/by-id") as $p) {
 		$r = realpath($p);
-		// Only /dev/sd* and /dev/hd* devices.
-		if (!is_bool(strpos($r, "/dev/sd")) || !is_bool(strpos($r, "/dev/hd"))) {
+		// Only /dev/sd*, /dev/hd* and /dev/nvme* devices.
+		if (!is_bool(strpos($r, "/dev/sd")) || !is_bool(strpos($r, "/dev/hd")) || !is_bool(strpos($r, "/dev/nvme"))) {
 			$paths[$r] = $p;
 		}
 	}
