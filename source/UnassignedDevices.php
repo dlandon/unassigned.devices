83105f5d2f1d10b9078a2a01ca7148a0e6ac0816
dlandon <dlandon@cinci.rr.com>
2017-09-30 17:05:40 -0400

    Add disk log button

--- source/UnassignedDevices.php
+++ source/UnassignedDevices.php
@@ -298,7 +298,7 @@ switch ($_POST['action']) {
 					echo "<td title='SMART Attributes on {$disk_name}'><img src='/webGui/images/".($disk_running ? "green-on.png":"green-blink.png" )."'>";
 					echo "<a href='/Main/New?name={$disk_name}'> {$disk_name}</a></td>";
 				}
-				echo "<td><a href="#" title="Disk Log Information" onclick="openBox('/webGui/scripts/disk_log&amp;arg1={$disk_name}','Disk Log Information',600,900,false);return false"><i class="fa fa-hdd-o icon"></i></a>{$hdd_serial}</td>";
+				echo "<td><a href=\"#\" title=\"Disk Log Information\" onclick=\"openBox('/webGui/scripts/disk_log&amp;arg1={$disk_name}','Disk Log Information',600,900,false);return false\"><i class='fa fa-hdd-o icon'></i></a>{$hdd_serial}</td>";
 				echo "<td class='mount'>{$mbutton}</td>";
 				echo "<td>{$temp}</td>";
 				echo ($p)?$p[5]:"<td>-</td>";
