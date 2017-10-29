f57df2e1617526a3d720fc14a2dfe62f70b41d9f
dlandon <dlandon@cinci.rr.com>
2017-10-28 20:08:23 -0400

    Change free disk space to 'df' value not calculated from size minus used

--- source/include/lib.php
+++ source/include/lib.php
@@ -1398,7 +1398,7 @@ function get_partition_info($device, $reload=FALSE){
 		$disk['target'] = str_replace("\\040", " ", trim(shell_exec("/bin/cat /proc/mounts 2>&1|/bin/grep ${device}|/bin/awk '{print $2}'")));
 		$disk['size']   = intval(trim(benchmark("shell_exec","/bin/lsblk -nb -o size ${device} 2>/dev/null")));
 		$disk['used']   = intval(trim(benchmark("shell_exec","/usr/bin/timeout 20 /bin/df '${device}' --output=used,source 2>/dev/null|/bin/grep -v 'Filesystem'|/bin/awk '{print $1}'")))*1024;
-		$disk['avail']  = $disk['size'] - $disk['used'];
+		$disk['avail']  = intval(trim(benchmark("shell_exec","/usr/bin/timeout 20 /bin/df '${device}' --output=avail,source 2>/dev/null|/bin/grep -v 'Filesystem'|/bin/awk '{print $1}'")))*1024;
 		if ( $disk['mountpoint'] = get_config($disk['serial'], "mountpoint.{$disk[part]}") ) {
 			if (! $disk['mountpoint'] ) goto empty_mountpoint;
 		} else {
