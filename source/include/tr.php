5ea0a4056780e1c643dedd905cf6cad88dbb74d0
dlandon <dlandon@familyoflandon.com>
2020-04-03 06:36:36 -0400

    Update tr.php with Squid changes.

--- source/include/tr.php
+++ source/include/tr.php
@@ -5,17 +5,14 @@
 
 $translations =   version_compare($unRaidSettings['version'],"6.9.0-beta1",">");
 if ( $translations ) {
+	require_once "$docroot/plugins/dynamix/include/Translations.php";
 	$translationFile = "$docroot/languages/{$_SESSION['locale']}/".basename(strtolower($_SERVER['REQUEST_URI'])).".txt";
-	$genericFile     = "$docroot/languages/{$_SESSION['locale']}/translations.txt";
-	$pluginTranslations = @parse_language($translationFile);
-	$genericTranslations = @parse_language($genericFile);
+	$genericFile = "$docroot/languages/{$_SESSION['locale']}/translations.txt";
+	$pluginTranslations = @parse_lang_file($translationFile);
+	$genericTranslations = @parse_lang_file($genericFile);
 	$language = array_merge(is_array($genericTranslations) ? $genericTranslations : [],is_array($pluginTranslations) ? $pluginTranslations : [] );
-	if ( empty($language) ) 
+	if ( empty($language) )
 		$translations = false;
-}
-
-function parse_language($file) {
-	return array_filter(parse_ini_string(preg_replace(['/"/m','/^(null|yes|no|true|false|on|off|none)=/mi','/^([^>].*)=([^"\'`].*)$/m','/^:((help|plug)\d*)$/m','/^:end$/m'],['\'','$1.=','$1="$2"',"_$1_=\"",'"'],str_replace("=\n","=''\n",file_get_contents($file)))),'strlen');
 }
 
 function tr($string,$ret=false) {
