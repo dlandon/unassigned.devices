f344baa74f06d6dd43fb9a5976902a23cb3b42c4
dlandon <dlandon@familyoflandon.com>
2021-05-01 10:45:22 -0400

    Add setting to enable/disable caching on remote mounts.

--- source/include/lib.php
+++ source/include/lib.php
@@ -840,12 +840,20 @@ function get_mount_params($fs, $dev, $ro = FALSE) {
 			break;
 
 		case 'cifs':
+			$enable_caching = "cache=none,";
+			if (get_config("Config", "favor_reliability") == "no") {
+				$enable_caching = "";
+			}
 			$credentials_file = "{$paths['credentials']}_".basename($dev);
-			return "cache=none,rw,nounix,iocharset=utf8,file_mode=0777,dir_mode=0777,uid=99,gid=100%s,credentials='$credentials_file'";
+			return $enable_caching."rw,nounix,iocharset=utf8,file_mode=0777,dir_mode=0777,uid=99,gid=100%s,credentials='$credentials_file'";
 			break;
 
 		case 'nfs':
-			return "rw,noac,hard,timeo=600,retrans=10";
+			$enable_caching = "noac,";
+			if (get_config("Config", "favor_reliability") == "no") {
+				$enable_caching = "";
+			}
+			return $enable_caching."rw,hard,timeo=600,retrans=10";
 			break;
 
 		default:
