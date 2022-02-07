50fb9fcce8b25e48eac499f082dbbf1d556f6da3
dlandon <dlandon@familyoflandon.com>
2022-02-07 06:39:48 -0600

    Add error and exception handler.

--- source/include/lib.php
+++ source/include/lib.php
@@ -10,6 +10,9 @@
  * all copies or substantial portions of the Software.
  */
 
+set_error_handler("log_error");
+set_exception_handler( "log_exception" );
+
 $plugin = "unassigned.devices";
 $paths = [	"smb_extra"			=> "/tmp/{$plugin}/smb-settings.conf",
 			"smb_usb_shares"	=> "/etc/samba/unassigned-shares",
@@ -172,6 +175,65 @@ function save_ini_file($file, $array, $save_config = true) {
 			@file_put_contents("/boot/config/plugins/".$plugin."/".basename($file), implode(PHP_EOL, $res));
 		}
 	}
+}
+
+/* Log program error. */
+function log_error($errno, $errstr, $errfile, $errline)
+{
+	switch($errno){
+	case E_ERROR:
+		$error = "Error";
+		break;
+	case E_WARNING:
+		$error = "Warning";
+		break;
+	case E_PARSE:
+		$error = "Parse Error";
+		break;
+	case E_NOTICE:
+		$error = "Notice";
+		return;
+		break;
+	case E_CORE_ERROR:
+		$error = "Core Error";
+		break;
+	case E_CORE_WARNING:
+		$error = "Core Warning";
+		break;
+	case E_COMPILE_ERROR:
+		$error = "Compile Error";
+		break;
+	case E_COMPILE_WARNING:
+		$error = "Compile Warning";
+		break;
+	case E_USER_ERROR:
+		$error = "User Error";
+		break;
+	case E_USER_WARNING:
+		$error = "User Warning";
+		break;
+	case E_USER_NOTICE:
+		$error = "User Notice";
+		break;
+	case E_STRICT:
+		$error = "Strict Notice";
+		break;
+	case E_RECOVERABLE_ERROR:
+		$error = "Recoverable Error";
+		break;
+	default:
+		$error = "Unknown error ($errno)";
+		return;
+		break;
+	}
+
+	unassigned_log("PHP {$error}: $errstr in {$errfile} on line {$errline}");
+}
+
+/* Log program exception. */
+function log_exception( $e )
+{
+	unassigned_log("PHP Exception: {$e->getMessage()} in {$e->getFile()} on line {$e->getLine()}");
 }
 
 /* Unassigned Devices logging. */
