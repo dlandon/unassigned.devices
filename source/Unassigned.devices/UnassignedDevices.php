beb86506b849cd03141f9b3bdf18902f9d2489a7
dlandon <dlandon@familyoflandon.com>
2022-06-23 08:09:26 -0500

    Add version check for disk log popup on 6.9,

--- source/Unassigned.devices/UnassignedDevices.php
+++ source/Unassigned.devices/UnassignedDevices.php
@@ -424,7 +424,13 @@ switch ($_POST['action']) {
 				$clear_disk		= ((get_config("Config", "destructive_mode") == "enabled") && ($p) && (! $mounted) && (! $disk['partitions'][0]['pool']) && (! $disk['partitions'][0]['disable_mount']) && (! $is_mounting) && (! $disk['partitions'][0]['pass_through']) && (! $disk['array_disk']) && (! $preclearing)) ? "<a device='{$partition['device']}' class='exec info' style='color:#CC0000;font-weight:bold;' onclick='clr_disk(this,\"{$partition['serial']}\",\"{$disk['device']}\");'><i class='fa fa-remove hdd'></i><span>"._("Clear Disk")."</span></a>" : "";
 
 				$disk_icon = $disk['ssd'] ? "icon-nvme" : "fa fa-hdd-o";
-				$hdd_serial = "<a class='info' href=\"#\" onclick=\"openTerminal('disklog', '{$disk_device}')\"><i class='".$disk_icon." icon'></i><span>"._("Disk Log Information")."</span></a>";
+				if (version_compare($version['version'],"6.9.9", ">")) {
+					/* Disk log in 6.10. */
+					$hdd_serial = "<a class='info' href=\"#\" onclick=\"openTerminal('disklog', '{$disk_device}')\"><i class='".$disk_icon." icon'></i><span>"._("Disk Log Information")."</span></a>";
+				} else {
+					/* Disk log in 6.9. */
+					$hdd_serial = "<a class='info' href=\"#\" onclick=\"openBox('/webGui/scripts/disk_log&amp;arg1={$disk_device}','Disk Log Information',600,900,false);return false\"><i class='".$disk_icon." icon'></i><span>"._("Disk Log Information")."</span></a>";
+				}
 				if ($p) {
 					$add_toggle = true;
 					if (! $disk['show_partitions']) {
