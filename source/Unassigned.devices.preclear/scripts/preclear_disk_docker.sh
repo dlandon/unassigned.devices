6ef261daf4ec8b660f8607dd028802a98a6e666b
dlandon <dlandon@familyoflandon.com>
2023-01-08 23:07:03 -0600

    Update Copyright dates.

--- source/Unassigned.devices.preclear/scripts/preclear_disk_docker.sh
+++ source/Unassigned.devices.preclear/scripts/preclear_disk_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2022, Dan Landon
+# Copyright 2023, Dan Landon
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License version 2,
