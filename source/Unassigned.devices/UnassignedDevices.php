ea2389b17ff620a059a4168f92158120d3f26f74
dlandon <dlandon@familyoflandon.com>
2023-02-05 08:39:27 -0600

    Fix device not showing in historical devices and disable remove historical device when disk is in standby.

--- source/Unassigned.devices/UnassignedDevices.php
+++ source/Unassigned.devices/UnassignedDevices.php
@@ -746,10 +746,14 @@ switch ($_POST['action']) {
 		/* Display the historical devices. */
 		foreach ($historical as $serial => $value) {
 			$is_standby = (new MiscUD)->get_device_host($serial);
-			$o_historical .= sprintf( "<tr><td><a class='info'><i class='fa fa-minus-circle orb %s'></i><span>"._("Historical Device is")." %s</span></a></td>", ( $is_standby ? "green-orb" : "grey-orb" ), ( $is_standby ? _("in standby") : _("offline") ));
+			$o_historical .= sprintf( "<tr><td><a class='info'><i class='fa fa-minus-circle orb %s'></i><span>"._("Historical Device is")." %s</span></a>".$historical[$serial]['display']."</td>", ( $is_standby ? "green-orb" : "grey-orb" ), ( $is_standby ? _("in standby") : _("offline") ));
 			$o_historical .= "<td>".$serial.$historical[$serial]['mountpoint']."</td>";
 			$o_historical .= "<td></td>";
-			$o_historical .= "<td><a style='color:#CC0000;font-weight:bold;cursor:pointer;' class='exec info' onclick='remove_disk_config(\"{$serial}\")'><i class='fa fa-remove hdd'></i><span>"._("Remove Device Configuration")."</span></a></td>";
+			if (! $is_standby) {
+				$o_historical .= "<td><a style='color:#CC0000;font-weight:bold;cursor:pointer;' class='exec info' onclick='remove_disk_config(\"{$serial}\")'><i class='fa fa-remove hdd' disabled></i><span>"._("Remove Device Configuration")."</span></a></td>";
+			} else {
+				$o_historical .= "<td><i class='fa fa-remove hdd' disabled></i></td>";
+			}
 			$o_historical .= "<td><a class='info' href='/Main/EditDeviceSettings?s=".$serial."&l=".$historical[$serial]['mntpoint']."&p="."1"."&t=true'><i class='fa fa-gears'></i><span>"._("Edit Historical Device Settings and Script")."</span></a></td>";
 			$o_historical .= "<td></td><td></td><td></td><td></td><td></td></tr>";
 		}
