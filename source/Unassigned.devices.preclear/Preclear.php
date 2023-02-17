745d636d558264aae358e78f161528f563e4b789
dlandon <dlandon@familyoflandon.com>
2023-02-17 07:47:19 -0600

    Remove php logging; fix php warnings.

--- source/Unassigned.devices.preclear/Preclear.php
+++ source/Unassigned.devices.preclear/Preclear.php
@@ -10,9 +10,6 @@
  * all copies or substantial portions of the Software.
  */
 
-set_error_handler("preclear_log_error");
-set_exception_handler( "preclear_log_exception" );
-
 $preclear_plugin	= "unassigned.devices.preclear";
 $docroot			= $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
 
@@ -47,70 +44,6 @@ if (! is_dir(dirname($state_file)) )
 #############	MISC FUNCTIONS		##############
 ##################################################
 
-function preclear_log_error($errno, $errstr, $errfile, $errline)
-{
-	switch($errno){
-	case E_ERROR:
-		$error = "Error";
-		break;
-	case E_WARNING:
-		$error = "Warning";
-		break;
-	case E_PARSE:
-		$error = "Parse Error";
-		break;
-	case E_NOTICE:
-		$error = "Notice";
-		return;
-		break;
-	case E_CORE_ERROR:
-		$error = "Core Error";
-		break;
-	case E_CORE_WARNING:
-		$error = "Core Warning";
-		break;
-	case E_COMPILE_ERROR:
-		$error = "Compile Error";
-		break;
-	case E_COMPILE_WARNING:
-		$error = "Compile Warning";
-		break;
-	case E_USER_ERROR:
-		$error = "User Error";
-		break;
-	case E_USER_WARNING:
-		$error = "User Warning";
-		break;
-	case E_USER_NOTICE:
-		$error = "User Notice";
-		break;
-	case E_STRICT:
-		$error = "Strict Notice";
-		break;
-	case E_RECOVERABLE_ERROR:
-		$error = "Recoverable Error";
-		break;
-	default:
-		$error = "Unknown error ($errno)";
-		return;
-		break;
-	}
-
-	preclear_log("PHP {$error}: $errstr in {$errfile} on line {$errline}");
-}
-
-function preclear_log_exception( $e )
-{
-	preclear_log("PHP Exception: {$e->getMessage()} in {$e->getFile()} on line {$e->getLine()}");
-}
-
-
-function _echo($m)
-{
-	echo "<pre>".print_r($m,TRUE)."</pre>";
-};
-
-
 function reload_partition($name)
 {
 	$device = "/dev/".$name;
@@ -120,7 +53,7 @@ function reload_partition($name)
 
 	/* Refresh partition information. */
 	exec("/usr/sbin/partprobe ".escapeshellarg($device));
-	
+
 	/* Update disk info. */
 	shell_exec("/sbin/udevadm trigger --action=change ".escapeshellarg($device));
 
@@ -375,6 +308,9 @@ if (isset($_POST['action'])) {
 		case 'stop_preclear':
 			$serials = is_array($_POST['serial']) ? $_POST['serial'] : [$_POST['serial']];
 			foreach ($serials as $serial) {
+				if ($serial == "DEVICE") {
+					continue;
+				}
 				$device = basename($Preclear->serialDisk($serial));
 				$session = "preclear_disk_{$serial}";
 
@@ -401,7 +337,7 @@ if (isset($_POST['action'])) {
 				}
 
 				TMUX::killSession($session);
-				sleep(0.2);
+				usleep(200 * 1000);
 				if (is_file($GLOBALS['preclear_status'].$device)) {
 					@unlink($GLOBALS['preclear_status'].$device);
 				}
