75fc1befc14858def4638b92bea8bcf2a625bc54
dlandon <dlandon@familyoflandon.com>
2023-02-26 06:28:56 -0600

    Remove php logging.

--- source/Unassigned.devices.preclear/logger.php
+++ source/Unassigned.devices.preclear/logger.php
@@ -12,9 +12,6 @@
 
 /* HEADER */
 
-set_error_handler("preclear_log_error");
-set_exception_handler( "preclear_log_exception" );
-
 $docroot		= $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
 $relative_path	= str_replace($docroot, '', __FILE__);
 require_once	"$docroot/webGui/include/ColorCoding.php";
@@ -22,62 +19,6 @@ require_once	"$docroot/webGui/include/ColorCoding.php";
 openlog(__FILE__, LOG_PID | LOG_PERROR, LOG_LOCAL0);
 
 /* FUNCTIONS */
-
-function preclear_log_error($errno, $errstr, $errfile, $errline)
-{
-	switch($errno){
-		case E_ERROR:
-			$error = "Error";
-			break;
-		case E_WARNING:
-			$error = "Warning";
-			break;
-		case E_PARSE:
-			$error = "Parse Error";
-			break;
-		case E_NOTICE:
-			$error = "Notice";
-			return; break;
-		case E_CORE_ERROR:
-			$error = "Core Error";
-			break;
-		case E_CORE_WARNING:
-			$error = "Core Warning";
-			break;
-		case E_COMPILE_ERROR:
-			$error = "Compile Error";
-			break;
-		case E_COMPILE_WARNING:
-			$error = "Compile Warning";
-			break;
-		case E_USER_ERROR:
-			$error = "User Error";
-			break;
-		case E_USER_WARNING:
-			$error = "User Warning";
-			break;
-		case E_USER_NOTICE:
-			$error = "User Notice";
-			break;
-		case E_STRICT:
-			$error = "Strict Notice";
-			break;
-		case E_RECOVERABLE_ERROR:
-			$error = "Recoverable Error";
-			break;
-		default:
-			$error = "Unknown error ($errno)";
-			return;
-			break;
-	}
-
-	preclear_log("PHP {$error}: $errstr in {$errfile} on line {$errline}");
-}
-
-function preclear_log_exception( $e )
-{
-	preclear_log("PHP Exception: {$e->getMessage()} in {$e->getFile()} on line {$e->getLine()}");
-}
 
 function preclear_log($msg) {
 	syslog(LOG_INFO, $msg);
