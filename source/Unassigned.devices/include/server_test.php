714d0a312b0b928f88665812af7aa70dad3359fb
dlandon <dlandon@familyoflandon.com>
2024-11-03 13:32:24 -0600

    Capitalize server name.

--- source/Unassigned.devices/include/server_test.php
+++ source/Unassigned.devices/include/server_test.php
@@ -6,7 +6,7 @@
 $plugin	= "unassigned.devices";
 $docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
 
-$server = $_POST['server'];
+$server = strtoupper($_POST['server']);
 
 /* Execute the get_ud_stats command to check SMB and NFS on a remote server. */
 $command = $docroot."/plugins/".$plugin."/scripts/get_ud_stats remote_test ".$server;
