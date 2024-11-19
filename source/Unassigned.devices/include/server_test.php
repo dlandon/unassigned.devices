b9c18827e07bd625e019fd724ea2e36a5e65eb07
dlandon <dlandon@familyoflandon.com>
2024-11-19 12:28:48 -0600

    Hard code the get_ud_stats script.

--- source/Unassigned.devices/include/server_test.php
+++ source/Unassigned.devices/include/server_test.php
@@ -1,15 +1,15 @@
 <?php
 /* 
- *  Execute get_ud_stats Command
+ *  Execute get_ud_stats Command TO TEST REMOTE SERVER
  */
 
-$plugin	= "unassigned.devices";
+
 $docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
 
 $server = strtoupper($_POST['server']);
 
 /* Execute the get_ud_stats command to check SMB and NFS on a remote server. */
-$command = $docroot."/plugins/".$plugin."/scripts/get_ud_stats remote_test ".$server;
+$command = $docroot."/plugins/unassigned.devices/scripts/get_ud_stats remote_test ".$server;
 
 echo shell_exec($command);
 ?>
